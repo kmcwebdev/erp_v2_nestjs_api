b7c1093df459529ed17554a901fe25db9f42fb30
Gelo <chrisgelosulit@gmail.com>
2023-09-05 10:36:37 +0800

    Refactor SQL query to include text search ranking and ordering by rank or created_at

--- src/finance/services/reimbursement.api.service.ts
+++ src/finance/services/reimbursement.api.service.ts
@@ -50,75 +50,75 @@ export class ReimbursementApiService {
     const { original_user_id } = user;
 
     const rawQuery = sql`SELECT 
-                frr.reimbursement_request_id,
-                frr.reference_no,
-                frrt.request_type,
-                fret.expense_type,
-                frrs.request_status,
-                frr.amount,
-                frr.attachment,
-                u.full_name,
-                u.email,
-                u.employee_id,
-                frr.date_approve
-                ${
-                  data?.text_search
-                    ? sql`
-                ,ts_rank(to_tsvector('english', coalesce(frr.text_search_properties, '')), websearch_to_tsquery(${data.text_search})) AS rank`
-                    : sql``
-                }  
-              FROM finance_reimbursement_requests as frr
-              INNER JOIN finance_reimbursement_request_types AS frrt
-                ON frrt.reimbursement_request_type_id = frr.reimbursement_request_type_id
-              INNER JOIN finance_reimbursement_expense_types AS fret
-                ON frr.expense_type_id = fret.expense_type_id
-              INNER JOIN finance_reimbursement_request_status AS frrs
-                ON frrs.request_status_id = frr.request_status_id
-              INNER JOIN users AS u
-                ON u.user_id = frr.requestor_id
-              WHERE frr.requestor_id = ${original_user_id}
-                ${
-                  data?.reimbursement_type_id
-                    ? sql`
-                AND frr.reimbursement_request_type_id = ${data.reimbursement_type_id}`
-                    : sql``
-                }
-                ${
-                  data?.expense_type_id
-                    ? sql`
-                AND frr.expense_type_id = ${data.expense_type_id}`
-                    : sql``
-                }
-                ${
-                  data?.request_status_id
-                    ? sql`
-                AND frr.request_status_id = ${data.request_status_id}`
-                    : sql``
-                }
-                ${
-                  data?.amount_min && data?.amount_max
-                    ? sql`
-                AND frr.amount BETWEEN ${data.amount_min} AND ${data.amount_max}`
-                    : sql``
-                }
-                ${
-                  data?.reference_no
-                    ? sql`
-                AND frr.reference_no = ${data.reference_no}`
-                    : sql``
-                }
-                ${
-                  data?.text_search
-                    ? sql`
-                AND to_tsvector('english', coalesce(frr.text_search_properties, '')) @@ websearch_to_tsquery(${data.text_search})`
-                    : sql``
-                }
+              frr.reimbursement_request_id,
+              frr.reference_no,
+              frrt.request_type,
+              fret.expense_type,
+              frrs.request_status,
+              frr.amount,
+              frr.attachment,
+              u.full_name,
+              u.email,
+              u.employee_id,
+              frr.date_approve
               ${
                 data?.text_search
-                  ? sql`ORDER BY rank DESC`
-                  : sql`ORDER BY frr.created_at DESC`
-              } LIMIT 10
-             `;
+                  ? sql`
+              ,ts_rank(to_tsvector('english', coalesce(frr.text_search_properties, '')), websearch_to_tsquery(${data.text_search})) AS rank`
+                  : sql``
+              }  
+            FROM finance_reimbursement_requests as frr
+            INNER JOIN finance_reimbursement_request_types AS frrt
+              ON frrt.reimbursement_request_type_id = frr.reimbursement_request_type_id
+            INNER JOIN finance_reimbursement_expense_types AS fret
+              ON frr.expense_type_id = fret.expense_type_id
+            INNER JOIN finance_reimbursement_request_status AS frrs
+              ON frrs.request_status_id = frr.request_status_id
+            INNER JOIN users AS u
+              ON u.user_id = frr.requestor_id
+            WHERE frr.requestor_id = ${original_user_id}
+              ${
+                data?.reimbursement_type_id
+                  ? sql`
+              AND frr.reimbursement_request_type_id = ${data.reimbursement_type_id}`
+                  : sql``
+              }
+              ${
+                data?.expense_type_id
+                  ? sql`
+              AND frr.expense_type_id = ${data.expense_type_id}`
+                  : sql``
+              }
+              ${
+                data?.request_status_id
+                  ? sql`
+              AND frr.request_status_id = ${data.request_status_id}`
+                  : sql``
+              }
+              ${
+                data?.amount_min && data?.amount_max
+                  ? sql`
+              AND frr.amount BETWEEN ${data.amount_min} AND ${data.amount_max}`
+                  : sql``
+              }
+              ${
+                data?.reference_no
+                  ? sql`
+              AND frr.reference_no = ${data.reference_no}`
+                  : sql``
+              }
+              ${
+                data?.text_search
+                  ? sql`
+              AND to_tsvector('english', coalesce(frr.text_search_properties, '')) @@ websearch_to_tsquery(${data.text_search})`
+                  : sql``
+              }
+            ${
+              data?.text_search
+                ? sql`ORDER BY rank DESC`
+                : sql`ORDER BY frr.created_at DESC`
+            } LIMIT 10
+            `;
 
     const execute = await rawQuery.execute(this.pgsql);
 
