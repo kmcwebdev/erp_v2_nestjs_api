23c02b149bb2644a0c3914af636e62358f2168ac
Gelo <chrisgelosulit@gmail.com>
2023-09-08 11:23:56 +0800

    Update reimbursement request data with new fields and values

--- src/finance/common/interface/reimbursement.interface.ts
+++ src/finance/common/interface/reimbursement.interface.ts
@@ -1,12 +1,21 @@
 export interface Reimbursement {
   reimbursement_request_id: string;
-  reimbursement_request_type_id: string;
-  expense_type_id: string;
   reference_no: string;
+  request_type_id: string;
+  request_type: string;
+  expense_type_id: string;
+  expense_type: string;
+  request_status_id: string;
+  request_status: string;
   requestor_id: string;
   attachment: string;
   amount: string;
-  request_status_id: string;
   date_approve: string | null;
+  dynamic_approvers: string[] | null;
+  user_id: string;
+  full_name: string | null;
+  client_name: string | null;
+  email: string | null;
+  employee_id: string | null;
   created_at: number;
 }
