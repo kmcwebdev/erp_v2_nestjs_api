12fc650946bb88164e831fd3a9174f49b702f97a
Gelo <chrisgelosulit@gmail.com>
2023-09-11 10:53:16 +0800

    Add reimbursement request interface and bulk approval service

---
+++ src/finance/common/interface/getOneRequest.interface.ts
@@ -0,0 +1,37 @@
+export interface ReimbursementRequest {
+  reimbursement_request_id: string;
+  request_type: string;
+  expense_type: string;
+  reference_no: string;
+  attachment: string;
+  attachment_mask_name: string;
+  amount: string;
+  request_status: string;
+  remarks: string | null;
+  requestor_id: string;
+  full_name: string | null;
+  email: string;
+  employee_id: string;
+  client_id: string;
+  client_name: string;
+  hrbp_approver_email: string;
+  dynamic_approvers: string[] | null;
+  date_approve: string | null;
+  next_approval_matrix_id: string;
+  next_approver_order: number;
+  created_at: string;
+  approvers: Approver[];
+}
+
+interface Approver {
+  approval_matrix_id: string;
+  approver_id: string;
+  approver_order: number;
+  has_approved: boolean;
+  has_rejected: boolean;
+  performed_by_user_id: string | null;
+  description: string | null;
+  approver_name: string | null;
+  is_group_of_approvers: boolean;
+  table_reference: string;
+}
