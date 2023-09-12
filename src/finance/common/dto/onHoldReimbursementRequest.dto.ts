e78c98bcd7857aca2abfc8f542ce93f71fcce708
Gelo <chrisgelosulit@gmail.com>
2023-09-12 14:22:49 +0800

    Update reimbursement request status to "on hold" with reason

--- src/finance/common/dto/onHoldReimbursementRequest.dto.ts
+++ src/finance/common/dto/onHoldReimbursementRequest.dto.ts
@@ -4,6 +4,7 @@ import { createZodDto } from 'nestjs-zod';
 // TODO: Add description to zod types
 const OnHoldReimbursementRequestSchema = z.object({
   reimbursement_request_id: z.string().uuid(),
+  onhold_reason: z.string().nonempty(),
 });
 
 export type OnHoldReimbursementRequestType = z.infer<
