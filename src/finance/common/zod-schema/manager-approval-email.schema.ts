4d538b5ca51db9952c0b21bcaeeb224660a62a47
Gelo <chrisgelosulit@gmail.com>
2023-09-21 12:32:18 +0800

    Add manager approval email schema and service

---
+++ src/finance/common/zod-schema/manager-approval-email.schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+// TODO: Add description to zod types
+export const ManagerApprovalEmailSchema = z.object({
+  to: z.array(z.string().email()),
+  fullName: z.string().nonempty(),
+  employeeId: z.string().nonempty(),
+  expenseType: z.string().nonempty(),
+  expenseDate: z.string().nonempty(),
+  amount: z.string().nonempty(),
+  receiptsAttached: z.string().nonempty(),
+});
+
+export type ManagerApprovalEmailType = z.infer<
+  typeof ManagerApprovalEmailSchema
+>;
